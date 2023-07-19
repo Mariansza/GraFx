d7f0f23671297af7b5a544d9df6c31ba23d3ce2a
Mariansza <122391604+Mariansza@users.noreply.github.com>
2023-07-19 15:31:14 +0200

    first try with a default doc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import StudioSDK from "@chili-publish/studio-sdk";
+import { defaultJSON } from "./default-doc";
 
 
-function initEditor() {
+async function initEditor() {
   const SDK = new StudioSDK({
     editorId: "studio-editor"
   });
 
   SDK.loadEditor();
   window.SDK = SDK;
+
+  await loadDocument(defaultJSON);
 }
+
+
+
+async function loadDocument(docJSON) {
+    if (docJSON) {
+        await window.SDK.document.load(docJSON);
+    } else {
+        await window.SDK.document.load("{}");
+    }
+
+}
+
 
 initEditor();
